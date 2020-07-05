e1d33fc11e98419cee632f1ac3fbfc1cfd0b325f
MaKyuSeok <mks502@naver.com>
2020-07-05 21:40:41 +0900

    BaekJoon Algorithm Q6603 로또 문제풀이
    	BOJ Q6603 로또 문제 풀이

---
+++ src/problem/backtracking/Main_6603.java
@@ -0,0 +1,53 @@
+package problem.backtracking;
+
+import java.util.Scanner;
+
+public class Main_6603 {
+    static boolean[] visited;
+    static final int MAX = 6;
+    static StringBuilder sb;
+    static int lotto[];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sb = new StringBuilder();
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0)
+                break;
+            lotto = new int[n];
+            visited = new boolean[n];
+            for (int i = 0; i < n; i++) {
+                lotto[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < lotto.length; i++) {
+                if (i + MAX > lotto.length)
+                    continue;
+
+                visited[i] = true;
+                backTracking(i, 1, lotto[i] + " ");
+                visited[i] = false;
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+
+    }
+
+
+    public static void backTracking(int index, int count, String str) {
+        if (index >= lotto.length || index + MAX - count > lotto.length) return;
+        if (count == MAX) {
+            sb.append(str + "\n");
+            return;
+        }
+        for (int i = index + 1; i < lotto.length; i++) {
+            if (visited[i])
+                continue;
+            visited[i] = true;
+            backTracking(i, count + 1, str + lotto[i] + " ");
+            visited[i] = false;
+        }
+    }
+}
