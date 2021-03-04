828d791433353ac697df4f0b1560c4c2e1a8fc03
MaKyuSeok <mks502@naver.com>
2021-03-04 16:09:28 +0900

    ADD boj Q.15650 N과M (2) 문제 풀이 포스팅
    brute
    brute force
    backtracking
    combination

---
+++ src/problem/brute/Main_15650.java
@@ -0,0 +1,49 @@
+package problem.brute;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_15650 {
+
+    static int n,m;
+    static boolean[] visited;
+    static int[] sequence;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        dfs(1, 1);
+    }
+
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] temp = br.readLine().split(" ");
+        n = Integer.parseInt(temp[0]);
+        m = Integer.parseInt(temp[1]);
+        sequence = new int[m+1];
+        visited = new boolean[n+1];
+    }
+
+    public static void dfs(int index, int depth) {
+        if(depth == m+1){
+            print();
+            return;
+        }
+        for(int i=index; i<=n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                sequence[depth] = i;
+                dfs(i,depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void print(){
+        for(int i=1; i<=m; i++){
+            System.out.print(sequence[i]+" ");
+        }
+        System.out.println();
+    }
+}
