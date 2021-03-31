97d520b9f6f37a4c316d37e6d5accd5065788819
MaKyuSeok <mks502@naver.com>
2021-04-01 00:44:11 +0900

    ADD boj Q.10942 팰린드롬? 문제 풀이
    Main_10942_1 string 의 substring 과 reverse 이용
    
    Main_10942_2 dynamic programming 이용
    
    string
    dp
    dynamic programming

---
+++ src/problem/dynamic/Q10942/Main_10942_1.java
@@ -0,0 +1,52 @@
+package problem.dynamic.Q10942;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10942_1 {
+
+    static String palindrome;
+    static int n,m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(sb.toString());
+    }
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        palindrome = br.readLine().replaceAll(" ","");
+        m = Integer.parseInt(br.readLine());
+        for(int i=0; i<m; i++){
+            String temp[] = br.readLine().split(" ");
+            int start = Integer.parseInt(temp[0]);
+            int end = Integer.parseInt(temp[1]);
+
+            if(end-start+1 == 2){
+                sb.append(0);
+                sb.append("\n");
+                continue;
+            }
+
+            String subString = palindrome.substring(start-1,end);
+            if (isPalindrome(subString) ){
+                sb.append(1);
+            }
+            else
+                sb.append(0);
+            sb.append("\n");
+        }
+        br.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        String reverseStr =  new StringBuilder(str).reverse().toString();
+        if (str.equals(reverseStr))
+            return true;
+        else
+            return false;
+    }
+}
---
+++ src/problem/dynamic/Q10942/Main_10942_2.java
@@ -0,0 +1,64 @@
+package problem.dynamic.Q10942;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10942_2 {
+
+    static int[] numbers;
+    static int dp[][];
+    static int n,m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(sb.toString());
+    }
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        String palindrome[] = br.readLine().split(" ");
+        numbers = new int[n+1];
+        dp = new int[n+1][n+1];
+        for(int i=1; i<=n; i++){
+            numbers[i] = Integer.parseInt(palindrome[i-1]);
+        }
+        solve();
+        m = Integer.parseInt(br.readLine());
+        for(int i=0; i<m; i++){
+            String temp[] = br.readLine().split(" ");
+            int start = Integer.parseInt(temp[0]);
+            int end = Integer.parseInt(temp[1]);
+            sb.append(dp[start][end]+"\n");
+        }
+        br.close();
+    }
+
+    public static void solve() {
+        for (int i = 1; i <= n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int i = 1; i <= n - 1; i++) {
+            if (numbers[i] == numbers[i + 1]) {
+                dp[i][i + 1] = 1;
+            } else {
+                dp[i][i + 1] = 0;
+            }
+        }
+
+
+        for (int j = 3; j <= n; j++) {
+            for (int i = 1; i <= j; i++) {
+                if (dp[i][j] == 1 || j - i == 1) {
+                    continue;
+                }
+                if (dp[i + 1][j - 1] == 1 && numbers[i] == numbers[j]) {
+                    dp[i][j] = 1;
+                }
+            }
+        }
+    }
+}
